433624ffe3470f8471f68195907d3b093e619563
Matys Laguerre <matys.laguerre@epitech.eu>
2025-06-04 18:12:21 +0200

    .

--- scripts/run_gamepad_if_idle.sh
+++ scripts/run_gamepad_if_idle.sh
@@ -4,30 +4,32 @@ BASE_DIR="/home/robocar/Robocar"
 LOGFILE="$BASE_DIR/logs/run_gamepad_if_idle.log"
 SERVICE_NAME="run_gamepad.service"
 
+YELLOW='\033[0;33m'
+RESET='\033[0m'
+
 mkdir -p "$(dirname "$LOGFILE")"
 
 {
-    echo "$(date) [INFO] Checking SSH session status..."
+    echo "$YELLOW$(date) Checking SSH session status...$RESET"
 
     SSH_USERS=$(who | grep -E "pts|ssh" | wc -l)
-    SSH_USERS="0"
 
     if [ "$SSH_USERS" -eq 0 ]; then
         if ! systemctl is-active --quiet "$SERVICE_NAME"; then
-            echo "$(date) [INFO] No SSH sessions detected. Starting $SERVICE_NAME service..."
+            echo "[INFO] No SSH sessions detected. Starting $SERVICE_NAME service..."
             sudo systemctl start "$SERVICE_NAME"
         else
-            echo "$(date) [INFO] No SSH users detected, but $SERVICE_NAME is already running."
+            echo "[INFO] No SSH users detected, but $SERVICE_NAME is already running."
         fi
     else
         if systemctl is-active --quiet "$SERVICE_NAME"; then
-            echo "$(date) [INFO] SSH users detected. Stopping $SERVICE_NAME service..."
+            echo "[INFO] SSH users detected. Stopping $SERVICE_NAME service..."
             sudo systemctl stop "$SERVICE_NAME"
         else
-            echo "$(date) [INFO] SSH users detected, but $SERVICE_NAME is not running."
+            echo "[INFO] SSH users detected, but $SERVICE_NAME is not running."
         fi
     fi
-    
+
 } >> "$LOGFILE" 2>&1
 
 exit 0
