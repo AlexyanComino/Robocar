6fba4dbb0f894a45e4cdd90d71afced01317a71f
Alexyan Comino <alexyan.comino@epitech.eu>
2025-06-20 03:58:11 +0200

    .

--- car.py
+++ car.py
@@ -124,6 +124,6 @@ class Car:
 
         :param actions: A dictionary containing 'throttle' and 'steering' values.
         """
-        print(actions)
+        print(self.get_speed())
         self.set_duty_cycle(actions['throttle'])
         self.set_servo(actions['steering'])
