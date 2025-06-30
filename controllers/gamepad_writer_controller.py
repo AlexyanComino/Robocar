37cd68c1ce670efa94b92039f21c0adbf939bb59
Matys Laguerre <matys.laguerre@epitech.eu>
2025-07-01 00:02:13 +0200

    fix: try to fix get_data.

--- controllers/gamepad_writer_controller.py
+++ controllers/gamepad_writer_controller.py
@@ -139,36 +139,70 @@ class GamepadWriterController(IController):
 
             data = dict.fromkeys(self.init_columns, 0.0)
 
-            ray_values = np.fromiter((rays_data[f"ray_{i}"] for i in range(self.num_rays)), dtype=float)
-
-            for i in range(self.num_rays):
-                data[f"ray_{i+1}"] = ray_values[i]
-
-            # Basic data
             data["speed"] = speed
-            data["delta_speed"] = speed - self.previous_data.get("speed", 0.0)
+            data["delta_speed"] = data["speed"] - self.previous_data.get("speed", 0.0)
             data["delta_steering"] = data["steering"] - self.previous_data.get("steering", 0.0)
 
-            # Closest ray and angle
-            closest_ray_index = ray_values.argmin()
+            # List of ray values
+            ray_values = np.array([rays_data[f"ray_{i+1}"] for i in range(self.num_rays)])
+
+            # Find the closest ray to the car
+            closest_ray_index = np.argmin(ray_values)
             angle_step = self.fov / (self.num_rays - 1)
-            data["angle_closest_ray"] = -self.fov / 2 + closest_ray_index * angle_step
+            data["angle_closest_ray"] = -(self.fov / 2) + closest_ray_index * angle_step
 
-            # Basic stats
-            data["avg_ray"] = ray_values.mean()
-            data["std_ray"] = ray_values.std()
-            data["min_ray"] = ray_values.min()
-            data["max_ray"] = ray_values.max()
+            # Calculate the average, standard deviation, min, and max of the ray values
+            data["avg_ray"] = np.mean(ray_values)
+            data["std_ray"] = np.std(ray_values)
+            data["min_ray"] = np.min(ray_values)
+            data["max_ray"] = np.max(ray_values)
 
-            n = self.num_rays
-            third = n // 3
-            data["avg_ray_left"] = ray_values[:third].mean()
-            data["avg_ray_center"] = ray_values[third:2*third].mean()
-            data["avg_ray_right"] = ray_values[2*third:].mean()
+            left_indices = range(self.num_rays // 3)
+            center_indices = range(self.num_rays // 3, 2 * self.num_rays // 3)
+            right_indices = range(2 * self.num_rays // 3, self.num_rays)
+
+            data["avg_ray_left"] = np.mean(ray_values[list(left_indices)])
+            data["avg_ray_center"] = np.mean(ray_values[list(center_indices)])
+            data["avg_ray_right"] = np.mean(ray_values[list(right_indices)])
+
             data["ray_balance"] = data["avg_ray_right"] - data["avg_ray_left"]
 
-            # Acceleration
-            data["acceleration"] = data["delta_speed"] - self.previous_data.get("delta_speed", 0.0)
+            # Acceleration calculation
+            prev_delta_speed = self.previous_data.get("delta_speed", 0.0)
+            data["acceleration"] = data["delta_speed"] - prev_delta_speed
+
+            # data = dict.fromkeys(self.init_columns, 0.0)
+
+            # ray_values = np.fromiter((rays_data[f"ray_{i}"] for i in range(self.num_rays)), dtype=float)
+
+            # for i in range(self.num_rays):
+            #     data[f"ray_{i+1}"] = ray_values[i]
+
+            # # Basic data
+            # data["speed"] = speed
+            # data["delta_speed"] = speed - self.previous_data.get("speed", 0.0)
+            # data["delta_steering"] = data["steering"] - self.previous_data.get("steering", 0.0)
+
+            # # Closest ray and angle
+            # closest_ray_index = ray_values.argmin()
+            # angle_step = self.fov / (self.num_rays - 1)
+            # data["angle_closest_ray"] = -self.fov / 2 + closest_ray_index * angle_step
+
+            # # Basic stats
+            # data["avg_ray"] = ray_values.mean()
+            # data["std_ray"] = ray_values.std()
+            # data["min_ray"] = ray_values.min()
+            # data["max_ray"] = ray_values.max()
+
+            # n = self.num_rays
+            # third = n // 3
+            # data["avg_ray_left"] = ray_values[:third].mean()
+            # data["avg_ray_center"] = ray_values[third:2*third].mean()
+            # data["avg_ray_right"] = ray_values[2*third:].mean()
+            # data["ray_balance"] = data["avg_ray_right"] - data["avg_ray_left"]
+
+            # # Acceleration
+            # data["acceleration"] = data["delta_speed"] - self.previous_data.get("delta_speed", 0.0)
 
             # Update state
             self.previous_data = data.copy()
